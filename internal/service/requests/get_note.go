a7ed1818f190e66dea4a7dbb82d13faa7fab5d93
user <alexsever2003@gmail.com>
2024-01-03 13:26:49 +0200

    :recycle: get request update

--- internal/service/requests/get_note.go
+++ internal/service/requests/get_note.go
@@ -1,12 +1,25 @@
 package requests
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
 )
 
 type GetNotesRequest struct {
+	ID int64
 }
 
 func NewGetNotesRequest(r *http.Request) (*GetNotesRequest, error) {
-	return &GetNotesRequest{}, nil
+	idStr := chi.URLParam(r, "id")
+	if idStr == "" {
+		return nil, errors.New("no note ID provided")
+	}
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return nil, errors.New("invalid note ID")
+	}
+	return &GetNotesRequest{ID: id}, nil
 }
