8c74ff46fd46fbbe3961b7bfb855336d6a321719
user <alexsever2003@gmail.com>
2024-01-04 14:23:24 +0200

    :recycle: updated code

--- internal/service/handlers/create_note.go
+++ internal/service/handlers/create_note.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/Distributed-Lab-Testing/example-svc/internal/data"
@@ -18,12 +19,21 @@ func CreateNoteHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err = ctx.DB(r).Notes().Insert(data.Note{
+	ids, err := ctx.DB(r).Notes().Insert(data.Note{
 		Content:   request.Data.Attributes.Content,
 		CreatedAt: request.Data.Attributes.CreatedAt,
 	})
 	if err != nil {
-		ctx.Log(r).WithError(err).Error("failed to insert new log")
+		ctx.Log(r).WithError(err).Error("failed to insert new note")
+		ape.RenderErr(w, problems.InternalError())
+		return
+	}
+
+	if len(ids) > 0 {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"id": ids[0]})
+	} else {
+		ctx.Log(r).Error("no ID returned after note insertion")
 		ape.RenderErr(w, problems.InternalError())
 		return
 	}
