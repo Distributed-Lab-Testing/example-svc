8411314c6930b0240d62f4ffbb824ba75eda02ad
user <alexsever2003@gmail.com>
2024-01-04 14:28:42 +0200

    :fire: removed comments

--- internal/service/requests/update_note.go
+++ internal/service/requests/update_note.go
@@ -10,35 +10,9 @@ import (
 )
 
 type UpdateNoteRequest struct {
-	ID   string         //`json:"-"`
+	ID   string
 	Data resources.Note `json:"data"`
 }
-
-// func NewUpdateNote(r *http.Request) (UpdateNoteRequest, error) {
-// 	noteIDParam := chi.URLParam(r, "id")
-// 	noteID, err := strconv.ParseInt(noteIDParam, 10, 64)
-
-// 	if err != nil {
-// 		return UpdateNoteRequest{}, err
-// 	}
-
-// 	request := UpdateNoteRequest{
-// 		ID: noteID,
-// 	}
-
-// 	if err := json.NewDecoder(r.Body).Decode(&request.Data); err != nil {
-// 		return request, err
-// 	}
-
-// 	return request, request.validate()
-// }
-
-// func (r UpdateNoteRequest) validate() error {
-// 	return validation.ValidateStruct(&r,
-// 		validation.Field(&r.ID, validation.Required),
-// 		validation.Field(&r.Data.Attributes.Content, validation.Required),
-// 	)
-// }
 
 func NewUpdateNote(r *http.Request) (UpdateNoteRequest, error) {
 	request := UpdateNoteRequest{
@@ -53,11 +27,5 @@ func NewUpdateNote(r *http.Request) (UpdateNoteRequest, error) {
 }
 
 func (r UpdateNoteRequest) validate() error {
-	return validation.Errors{
-		"user_id": validation.Validate(
-			&r.ID,
-			validation.Required,
-			// is.UUID,
-		),
-	}.Filter()
+	return validation.Errors{}.Filter()
 }
