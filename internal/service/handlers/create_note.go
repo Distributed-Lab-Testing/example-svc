a9c67e7f1546b802ee46d9a52d2e1b53925f0d03
user <alexsever2003@gmail.com>
2024-01-05 16:25:59 +0200

    :recycle: code changed

--- internal/service/handlers/create_note.go
+++ internal/service/handlers/create_note.go
@@ -1,12 +1,12 @@
 package handlers
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/Distributed-Lab-Testing/example-svc/internal/data"
 	"github.com/Distributed-Lab-Testing/example-svc/internal/service/ctx"
 	"github.com/Distributed-Lab-Testing/example-svc/internal/service/requests"
+	"github.com/Distributed-Lab-Testing/example-svc/resources"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
 )
@@ -28,15 +28,12 @@ func CreateNoteHandler(w http.ResponseWriter, r *http.Request) {
 		ape.RenderErr(w, problems.InternalError())
 		return
 	}
-
-	if len(ids) > 0 {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{"id": ids[0]})
-	} else {
+	if len(ids) == 0 {
 		ctx.Log(r).Error("no ID returned after note insertion")
 		ape.RenderErr(w, problems.InternalError())
 		return
 	}
-
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	ape.Render(w, resources.NewIdResponse(resources.NewKeyInt64(ids[0], resources.NOTES)))
 }
